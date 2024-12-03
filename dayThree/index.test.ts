19d844ce198b03e9ae8fb9087e552359b7cbaaaa
the_dongle <stevewalkerbeta@gmail.com>
2024-12-03 14:04:58 +0000

    Day Two part two complete

--- dayThree/index.test.ts
+++ dayThree/index.test.ts
@@ -1,10 +1,11 @@
 import { expect } from "@std/expect";
 import { describe, it } from "@std/testing/bdd";
 import {
+  inBetweenDosPattern,
   mulOverAndOver,
   mulPattern,
   mulWrapper,
-  numInMullPattern,
+  numInMulPattern,
 } from "./index.ts";
 import answer from "../spoilers.json" with { type: "json" };
 
@@ -16,7 +17,6 @@ const multTest = {
 describe("Mul() Pattern", () => {
   it(`gets regexp array of length ${multTest.output} from example`, () => {
     const expected = multTest.input.match(mulPattern);
-    console.log(expected);
     expect(expected).toHaveLength(4);
   });
 });
@@ -28,7 +28,7 @@ const digitTest = {
 
 describe("Num in Mul Pattern", () => {
   it(`gets numbers ${digitTest.output} from ${digitTest.input}`, () => {
-    const expected = digitTest.input.match(numInMullPattern);
+    const expected = digitTest.input.match(numInMulPattern);
     expect(expected).toMatchObject({ ...digitTest.output });
   });
 });
@@ -65,7 +65,48 @@ const partOneTest: { name: string; input: string; output: number }[] = [{
 describe("Part one - Main", () => {
   partOneTest.forEach((t) => {
     it(`returns ${t.output} from ${t.name}`, () => {
-      expect(mulOverAndOver(t.input)).toBe(t.output);
+      expect(mulOverAndOver(t.input, false)).toBe(t.output);
+    });
+  });
+});
+
+const dosAnddontsTests: { name: string; input: string; length: number }[] = [{
+  name: "tutorial example 2",
+  input: Deno.readTextFileSync("./dayThree/example2.txt"),
+  length: 2,
+}, {
+  name: "Custom test",
+  input: Deno.readTextFileSync("./dayThree/customTest.txt"),
+  length: 2,
+}];
+
+describe("In between dos pattern", () => {
+  dosAnddontsTests.forEach((t) => {
+    const expected = t.input.match(inBetweenDosPattern);
+    it(`retrieves sections of length ${t.name} from ${t.name}`, () => {
+      expect(expected).toHaveLength(t.length);
+    });
+  });
+});
+
+const partTwoTest: { name: string; input: string; output: number }[] = [{
+  name: "tutorial example",
+  input: "./dayThree/example2.txt",
+  output: 48,
+}, {
+  name: "puzzle input",
+  input: "./dayThree/puzzle1.txt",
+  output: answer["3"]["2"],
+}, {
+  name: "custom test 1",
+  input: "./dayThree/customTest.txt",
+  output: 14,
+}];
+
+describe("Part Two - Main", () => {
+  partTwoTest.forEach((t) => {
+    it(`returns ${t.output} from ${t.name}`, () => {
+      expect(mulOverAndOver(t.input, true)).toBe(t.output);
     });
   });
 });
--- dayThree/index.ts
+++ dayThree/index.ts
@@ -2,17 +2,30 @@ import matchEach from "../dayOne/matcher.ts";
 
 export const mulPattern = /mul\(\d{1,3},\d{1,3}\)/g;
 
-export const numInMullPattern = /\d+/g;
+export const numInMulPattern = /\d+/g;
 
-export const inBetweenDosPattern = /(do\(\)|^).*?(don't\(\)|$)/
+export const inBetweenDosPattern = /(do\(\)|^).*?(don't\(\)|$)/gs
+
+export const numInMullPattern = /\d+/g;
 
 export function mulWrapper(mulFn: string): number {
   const [num1, num2] = mulFn.match(numInMullPattern) ?? ["0", "0"];
   return parseInt(num1) * parseInt(num2);
 }
 
-export function mulOverAndOver(path: string): number {
-  const text = Deno.readTextFileSync(path);
+function mullOver(text: string, pattern: RegExp): string {
+  let filtered = "";
+  matchEach(text, pattern, (v) => {
+    filtered += v;
+  });
+  return filtered;
+}
+
+export function mulOverAndOver(path: string, filter: boolean): number {
+  let text = Deno.readTextFileSync(path);
+  if (filter) {
+    text = mullOver(text, inBetweenDosPattern);
+  }
   let sum = 0;
   matchEach(text, mulPattern, (v) => {
     sum += mulWrapper(v);
