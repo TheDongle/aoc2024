adfb2c7ffe8dfb6874c662eec6efd2ee25cb1f18
the_dongle <stevewalkerbeta@gmail.com>
2024-12-06 20:57:09 +0000

    day 5 part 2 complete

--- day5/index.test.ts
+++ day5/index.test.ts
@@ -1,25 +1,67 @@
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
-import { menInTheMiddle, parseData, validMiddlePage } from "./index.ts";
+import {
+  default as mainMan,
+  manMiddle,
+  sortMan,
+  defineMan,
+} from "./index.ts";
 import answer from "../spoilers.json" with { type: "json" };
 
 const manTest = [61, 53, 29, 0, 0, 0];
 
+const { rules, manuals } = defineMan("./day5/example.txt");
+
 describe("single Manual result", () => {
-  const { rules, manuals } = parseData("./day5/example.txt");
   manTest.forEach((v, i) => {
     const man = manuals[i];
     it(`returns ${v} from row ${man}`, () => {
-      expect(validMiddlePage(rules, man)).toBe(v);
+      expect(manMiddle(rules, man, "ordered")).toBe(v);
     });
   });
 });
 
 describe("Part One", () => {
   it("Solves the tutorial", () => {
-    expect(menInTheMiddle("./day5/example.txt")).toBe(143);
+    expect(mainMan("./day5/example.txt", "ordered")).toBe(143);
   });
   it("Solves the puzzle", () => {
-    expect(menInTheMiddle("./day5/puzzle.txt")).toBe(answer[5][1]);
+    expect(mainMan("./day5/puzzle.txt", "ordered")).toBe(
+      answer["5"]["1"],
+    );
+  });
+});
+
+describe("Part Two", () => {
+  it("Solves the tutorial", () => {
+    expect(mainMan("./day5/example.txt", "unordered")).toBe(123);
+  });
+  it("Solves the puzzle", () => {
+    expect(mainMan("./day5/puzzle.txt", "unordered")).toBe(
+      answer["5"]["2"],
+    );
+  });
+});
+
+const sortTests: { input: number[]; output: number[] }[] = [
+  {
+    input: [75, 97, 47, 61, 53],
+    output: [97, 75, 47, 61, 53],
+  },
+  {
+    input: [61, 13, 29],
+    output: [61, 29, 13],
+  },
+  {
+    input: [97, 13, 75, 29, 47],
+    output: [97, 75, 47, 29, 13],
+  },
+];
+
+describe("Sort tests", () => {
+  sortTests.forEach((t) => {
+    it(`sorts ${t.input} into ${t.output}`, () => {
+      expect(sortMan(t.input, rules)).toMatchObject({ ...t.output });
+    });
   });
 });
--- day5/index.ts
+++ day5/index.ts
@@ -1,14 +1,9 @@
 import TextParser from "../day2/parse.ts";
 
-// Only Part One finished today
-
-// Note for part two - Sorting!
-
-
 type Rules = Map<number, Set<number>>;
 type Manuals = number[][];
 
-export function parseData(path: string): { rules: Rules; manuals: Manuals } {
+function defineMan(path: string): { rules: Rules; manuals: Manuals } {
   const data = new TextParser(path).numberArrays();
   const splitPoint = data.findIndex((x) => x.length === 0);
   const rules: Rules = new Map();
@@ -18,26 +13,39 @@ export function parseData(path: string): { rules: Rules; manuals: Manuals } {
   return { rules, manuals: data.slice(splitPoint + 1) };
 }
 
-export function validMiddlePage(rules: Rules, manual: number[]): number {
-  const ruleObserved = new Map<number, number>();
-  const pageSeen = new Set<number>();
-  for (let i = 0; i < manual.length; i++) {
-    const page = manual[i];
-    const pageRules = rules.get(page) ?? new Set<number>();
-    if (
-      i < (ruleObserved.get(page) ?? 0) || !pageRules.isDisjointFrom(pageSeen)
-    ) {
-      return 0;
-    }
-    pageRules.forEach((rule) =>
-      ruleObserved.set(rule, (ruleObserved.get(rule) ?? 0) + 1)
-    );
-    pageSeen.add(page);
+const sortMan = (manual: number[], rules: Rules): number[] =>
+  manual.toSorted((a, b) =>
+    rules.get(a)?.has(b) ? -1 : rules.get(b)?.has(a) ? 1 : 0
+  );
+
+function manMiddle(
+  rules: Rules,
+  manual: number[],
+  middlesOf: "ordered" | "unordered",
+): number {
+  const sorted = sortMan(manual, rules);
+  if (manEqual(sorted, manual)) {
+    return middlesOf === "ordered" ? sorted[Math.floor(manual.length / 2)] : 0;
   }
-  return manual[Math.floor(manual.length / 2)];
+  return middlesOf === "unordered" ? sorted[Math.floor(manual.length / 2)] : 0;
 }
 
-export function menInTheMiddle(path: string): number {
-  const { manuals, rules } = parseData(path);
-  return manuals.reduce((a, b) => a + validMiddlePage(rules, b), 0);
+function manEqual(a: number[], b: number[]): boolean {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+function mainMan(
+  path: string,
+  middlesOf: "ordered" | "unordered",
+): number {
+  const { manuals, rules } = defineMan(path);
+  return manuals.reduce((a, b) => a + manMiddle(rules, b, middlesOf), 0);
+}
+
+export { manMiddle, sortMan, defineMan };
+export default mainMan;
