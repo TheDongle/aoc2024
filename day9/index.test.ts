f30b3f1b83a36563c44778d9973231c3a58f8037
the_dongle <stevewalkerbeta@gmail.com>
2025-01-05 15:53:51 +0000

    day 9 part two started

--- day9/index.test.ts
+++ day9/index.test.ts
@@ -1,57 +1,66 @@
 import {
-  compactFiles,
-  decodeDiskMap,
   default as day9,
-  updateFileSystemChecksum,
 } from "./index.ts";
 import { describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import answer from "../spoilers.json" with { type: "json" };
 
-describe("Decode Disk Map", () => {
-  const input = Deno.readTextFileSync("./day9/example.txt");
-  it(`outputs decoded disk map from tutorial`, () => {
-    expect({ ...decodeDiskMap(input) }).toMatchObject({
-      ...Array.from(
-        "00...111...2...333.44.5555.6666.777.888899".split(""),
-        (v) => /\d/.test(v) ? parseInt(v) : undefined,
-      ),
-    });
-  });
-});
+// describe("Decode Disk Map", () => {
+//   const input = Deno.readTextFileSync("./day9/example.txt");
+//   it(`outputs decoded disk map from tutorial`, () => {
+//     expect({ ...decodeDiskMap(input) }).toMatchObject({
+//       ...Array.from(
+//         "00...111...2...333.44.5555.6666.777.888899".split(""),
+//         (v) => /\d/.test(v) ? parseInt(v) : undefined,
+//       ),
+//     });
+//   });
+// });
 
-const compactTestInput = Array.from(
-  "00...111...2...333.44.5555.6666.777.888899".split(""),
-  (v) => /\d/.test(v) ? parseInt(v) : undefined,
-);
+// describe("Draw Graph", () => {
+//   const map = drawFileSizeGraph("2020305");
+//   const keys = [...map.keys()];
+//   const vals = [...map.values()];
+//   it(`ordered last size as first`, () => {
+//     expect(keys[0]).toBe(5);
+//   });
+//   it(`ordered first size as last key`, () => {
+//     expect(keys.at(-1)).toBe(2);
+//   });
+// });
 
-describe("Compact files", () => {
-  it(`outputs compacted file system from tutorial`, () => {
-    expect(
-      compactFiles(compactTestInput).join("").padEnd(
-        compactTestInput.length,
-        ".",
-      ),
-    ).toBe(
-      "0099811188827773336446555566..............",
-    );
-  });
-});
+// const compactTestInput = Array.from(
+//   "00...111...2...333.44.5555.6666.777.888899".split(""),
+//   (v) => /\d/.test(v) ? parseInt(v) : undefined,
+// );
 
-describe("Update File System checksum", () => {
-  it(`outputs total from tutorial`, () => {
-    expect(
-      updateFileSystemChecksum(
-        Array.from(
-          "0099811188827773336446555566..............".split(""),
-          (v) => /\d/.test(v) ? parseInt(v) : undefined,
-        ),
-      ),
-    ).toBe(
-      1928,
-    );
-  });
-});
+// describe("Compact files", () => {
+//   it(`outputs compacted file system from tutorial`, () => {
+//     expect(
+//       compactFiles(compactTestInput).join("").padEnd(
+//         compactTestInput.length,
+//         ".",
+//       ),
+//     ).toBe(
+//       "0099811188827773336446555566..............",
+//     );
+//   });
+// });
+
+// describe("Update File System checksum", () => {
+//   it(`outputs total from tutorial`, () => {
+//     expect(
+//       updateFileSystemChecksum(
+//         Array.from(
+//           "0099811188827773336446555566..............".split(""),
+//           (v) => /\d/.test(v) ? parseInt(v) : undefined,
+//         ),
+//       ),
+//     ).toBe(
+//       1928,
+//     );
+//   });
+// });
 
 const mainTests: { input: string; name: string; output: number }[] = [
   {
@@ -73,3 +82,24 @@ describe("Day 9 part 1", () => {
     });
   });
 });
+
+// const mainTests2: { input: string; name: string; output: number }[] = [
+//   {
+//     name: "tutorial",
+//     input: "./day9/example.txt",
+//     output: 2858,
+//   },
+//   {
+//     name: "puzzle",
+//     input: "./day9/puzzle.txt",
+//     output: answer["9"]["2"],
+//   },
+// ];
+
+// describe("Day 9 part 2", () => {
+//   mainTests2.forEach((t) => {
+//     it(`${t.name} sample returns ${t.output}`, () => {
+//       expect(day9Part2(t.input)).toBe(t.output);
+//     });
+//   });
+// });
--- day9/index.ts
+++ day9/index.ts
@@ -1,24 +1,107 @@
 type HardDrive = (number | undefined)[];
 
+function decodeBlock(
+  firstCharacter: string,
+  firstCharacterIndex: number,
+  secondCharacter?: string,
+): HardDrive {
+  const blockId = parseInt(`${Math.floor(firstCharacterIndex / 2)}`);
+  const memoryInUse = parseInt(firstCharacter);
+  const memoryFree = parseInt(secondCharacter || "0");
+  const blockLength = memoryInUse + memoryFree;
+  return Array(blockLength).fill(blockId, 0, blockLength - memoryFree);
+}
+
+// function decodeFreeMemory(freeMemoryCharacter: string): undefined[] {
+//   return Array(parseInt(freeMemoryCharacter || "0"));
+// }
+
 function decodeDiskMap(diskMapInput: string): HardDrive {
   const decodedDiskMapOutput: HardDrive = [];
 
   for (let index = 0; index < diskMapInput.length; index += 2) {
-    const blockId = parseInt(`${Math.floor(index / 2)}`);
-    const memoryInUse = parseInt(diskMapInput[index]);
-    const memoryFree = parseInt(diskMapInput[index + 1]) || 0;
-
-    const blockLength = memoryInUse + memoryFree;
-    const nextBlock = Array(blockLength).fill(
-      blockId,
-      0,
-      blockLength - memoryFree,
+    decodedDiskMapOutput.push(
+      ...decodeBlock(diskMapInput[index], index, diskMapInput[index + 1]),
     );
-
-    decodedDiskMapOutput.push(...nextBlock);
   }
   return decodedDiskMapOutput;
 }
+
+// type SizeToLocationGraph = Map<number, number[]>;
+
+// const examineGraph = (spaceFree: number, graph: SizeToLocationGraph) => {
+//   for (const key of graph.keys()) {
+//     if (key <= spaceFree) {
+//       const values = graph.get(key)!;
+//       const ele = values.shift()!;
+//       if (values.length === 0) {
+//         graph.delete(key);
+//       } else {
+//         graph.set(key, values);
+//       }
+//       return ele;
+//     }
+//   }
+//   return -1;
+// };
+
+// const makeUnvisitedSet = (diskMapInput: string): Map<number, number> => {
+//   const unvisited = new Map<number, number>();
+//   for (let i = diskMapInput.length - 1; i >= 0; i -= 2) {
+//     unvisited.set(i, parseInt(diskMapInput[i]));
+//   }
+//   return unvisited;
+// };
+
+// const checkSet = (spaceFree: number, set: Map<number, number>) => {
+//   for (const [key, val] of set.entries()) {
+//     if (val <= spaceFree) {
+//       set.delete(key);
+//       return key;
+//     }
+//   }
+//   return -1;
+// };
+
+// function decodeAndCompactDisk(diskMapInput: string): HardDrive {
+//   const decodedStart: HardDrive = [];
+
+//   const unvisitedSet = makeUnvisitedSet(diskMapInput);
+
+//   const freeSpace: undefined[] = [];
+
+//   for (let index = 0; index < diskMapInput.length; index += 2) {
+//     if (unvisitedSet.has(index)) {
+//       unvisitedSet.delete(index);
+
+//       const spaceChar = diskMapInput[index + 1];
+
+//       const leftBlock = decodeBlock(diskMapInput[index], index);
+
+//       const startingSpace = parseInt(spaceChar) || 0;
+
+//       while (startingSpace - leftBlock.length > 0) {
+//         const nextBlockIndex = checkSet(
+//           startingSpace - leftBlock.length,
+//           unvisitedSet,
+//         );
+//         if (nextBlockIndex === -1) {
+//           break;
+//         }
+//         const nextBlock = decodeBlock(
+//           diskMapInput[nextBlockIndex],
+//           nextBlockIndex,
+//         );
+
+//         leftBlock.push(...nextBlock);
+
+//         freeSpace.length += parseInt(diskMapInput[nextBlockIndex + 1]) || 0;
+//       }
+//       decodedStart.push(...leftBlock);
+//     }
+//   }
+//   return decodedStart.concat(freeSpace);
+// }
 
 function compactFiles(sparseDrive: HardDrive): HardDrive {
   const compactDrive = sparseDrive.slice();
@@ -57,6 +140,13 @@ function day9(pathToFile: string): number {
     compactFiles(decodeDiskMap(Deno.readTextFileSync(pathToFile))),
   );
 }
+
+// function day9Part2(pathToFile: string): number {
+//   return updateFileSystemChecksum(
+//     decodeAndCompactDisk(Deno.readTextFileSync(pathToFile)),
+//   );
+// }
+
 
 export { compactFiles, decodeDiskMap, updateFileSystemChecksum };
 
